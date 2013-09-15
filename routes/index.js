445b1473db9554e56dcf405fd6c5cb286bb68586
Kohei Shingai <koheishingai@gmail.com>
2013-09-15 16:27:22 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-
-/*
- * GET home page.
- */
+var io = require('socket.io').listen(3001);
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+io.sockets.on('connection', function (socket) {
+socket.emit('examples', 'aaa');
+socket.on('on', function (data) {
+    console.log(data);
+});
+
+});
