9b5b609ae60197e979bb79b54c99447bcbf55dbe
Kohei Shingai <koheishingai@gmail.com>
2013-09-19 04:06:00 +0900

    Commit

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
+
 io.sockets.on('connection', function (socket) {
 socket.emit('examples', 'aaa');
 socket.on('on', function (data) {
